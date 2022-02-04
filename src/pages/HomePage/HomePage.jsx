375704810c58a9cc604828ffd8b8cd3eb236a111
Allen Xie <allenxie@Allens-MacBook-Pro.local>
2022-02-03 20:39:27 -0800

    Create search bar on home page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,27 +1,36 @@
 import { useState } from "react";
 import 'antd/dist/antd.css';
 import "./HomePage.css";
-import { Row, Col, Carousel, Radio } from 'antd';
+import { Row, Col, Carousel, Input } from 'antd';
+
 
 
 export default function HomePage () {
+
+    const {Search} = Input
     
     return (
-        <Row>
-            <Col>
-                <Carousel autoplay className="carousel">
-                    <div>
-                        <span className="carouselItem">hi hey </span>
-                    </div>
-                    <div>
-                        <span className="carouselItem">yo</span>
-                    </div>
-                    <div>
-                        <span className="carouselItem">beeee</span>
-                    </div>
-                </Carousel>
-            </Col>
-        
-        </Row>
+        <>
+            <Row>
+                <Col span={12} offset={6}>
+                    <Search className="searchInput" placeholder="Search product name..."/>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <Carousel autoplay className="carousel">
+                        <div>
+                            <span className="carouselItem">hi hey </span>
+                        </div>
+                        <div>
+                            <span className="carouselItem">yo</span>
+                        </div>
+                        <div>
+                            <span className="carouselItem">beeee</span>
+                        </div>
+                    </Carousel>
+                </Col>
+            </Row>
+        </>
     )
 }
