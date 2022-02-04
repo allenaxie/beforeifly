7f0ed925fbbba7fa0e23842f4bec84c7acf1998a
Allen Xie <allenxie@Allens-MacBook-Pro.local>
2022-02-03 21:31:05 -0800

    Create product grid in home page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,13 +1,15 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 import 'antd/dist/antd.css';
 import "./HomePage.css";
-import { Row, Col, Carousel, Input } from 'antd';
+import { Row, Col, Carousel, Input, Card } from 'antd';
 
 
 
 export default function HomePage () {
 
     const {Search} = Input
+    const {Meta} = Card;
     
     return (
         <>
@@ -30,6 +32,50 @@ export default function HomePage () {
                         </div>
                     </Carousel>
                 </Col>
+            </Row>
+            <Row className="browseLinkContainer">
+                <Col span={12} offset={6}>
+                    <Link className="browseItemsLink" to="/products">Browse more items...</Link>
+                </Col>
+            </Row>
+            <Row gutter={[16,16]}>
+                <Col span={12}>
+                    <Card 
+                    hoverable>
+                        <Meta
+                            title="Product Title"
+                            description = "description will go here"
+                        />
+                    </Card>
+                </Col>
+                <Col span={12}>
+                    <Card 
+                    hoverable>
+                        <Meta
+                            title="Product Title"
+                            description = "description will go here"
+                        />
+                    </Card>
+                </Col>
+                <Col span={12}>
+                    <Card 
+                    hoverable>
+                        <Meta
+                            title="Product Title"
+                            description = "description will go here"
+                        />
+                    </Card>
+                </Col>
+                <Col span={12}>
+                    <Card 
+                    hoverable>
+                        <Meta
+                            title="Product Title"
+                            description = "description will go here"
+                        />
+                    </Card>
+                </Col>
+                
             </Row>
         </>
     )
