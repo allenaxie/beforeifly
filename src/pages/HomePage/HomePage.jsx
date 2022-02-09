91fbede480eff32a991e4ed5beb980e51ddd9c9a
Allen Xie <allenxie@Allens-MacBook-Pro.local>
2022-02-08 22:40:39 -0800

    Center first carousel banner on all breakpoints

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -45,9 +45,16 @@ export default function HomePage({ productItems, setProductItems ,cart, setCart 
                 </Row>
                 <Row>
                     <Col>
-                        <Carousel autoplay className="carousel">
+                        <Carousel 
+                        autoplay 
+                        className="carousel">
                             <Row className="carouselItem1">
-                                <Col span={12} offset={6}>
+                                <Col 
+                                xs={{span:18, offset:3}}
+                                sm={{span:16, offset:5}}
+                                md={{span:12, offset:7}}
+                                lg={{span:12, offset:9}}
+                                >
                                     <img className="banner" src="images/logo-banner.png"></img>
                                 </Col>
                             </Row>
