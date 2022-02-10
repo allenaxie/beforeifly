fc42c6d725dc7e906765c5a953ac0771acd823f9
Allen Xie <allenxie@Allens-MacBook-Pro.local>
2022-02-10 11:48:36 -0800

    Update empty order history page. Add image/text and a button to redirect to /products

--- src/pages/OrderHistoryPage/OrderHistoryPage.jsx
+++ src/pages/OrderHistoryPage/OrderHistoryPage.jsx
@@ -1,12 +1,17 @@
 import { useState, useEffect } from "react";
 import * as ordersAPI from "../../utilities/orders-api";
 import OrderList from "../../components/OrderList/OrderList";
+import { Typography, Result, Button } from "antd";
+import { useNavigate } from "react-router-dom" 
 
 
 export default function OrderHistoryPage({user, setUser}) {
   
   const [orderHistory, setOrderHistory] = useState([])
   const [activeOrder, setActiveOrder] = useState(orderHistory[0])
+  const navigate = useNavigate();
+  const { Title } = Typography;
+
 
   useEffect(function() {
     async function getOrders() {
@@ -19,12 +24,22 @@ export default function OrderHistoryPage({user, setUser}) {
 
   console.log('orderHistory',orderHistory);
 
+  function handleClick () {
+    navigate('/products')
+  }
 
 
   return (
     <div>
-      <h1>OrderHistoryPage</h1>
-      <OrderList ordersList={orderHistory} />
+
+      {orderHistory.length > 0 ? <OrderList ordersList={orderHistory} /> :
+      <Result
+      status="404"
+      title= "Looks like you haven't made any orders yet"
+      extra= {<Button type="primary" onClick={handleClick}>Start Shopping Now!</Button>}
+      />
+      
+      }
     </div>
   )
 }
